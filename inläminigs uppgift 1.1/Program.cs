d5b8921993176e92f8705120b1db770a82aa3c68
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:01:48 +0200

    Add project files.

---
+++ inläminigs uppgift 1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+namespace Uppgift_1._1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");// visar upp texten som står innanför paranteserna
+            Console.WriteLine("Du har lärt dig skriva ut tecken som och");//visar upp texten som står innanför paranteserna
+            Console.ReadKey();
+        }
+    }
+}
